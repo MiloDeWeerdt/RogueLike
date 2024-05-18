8db7c9d004e79bd302addec8af7218c5d1026f29
MiloDeWeerdt <144011460+MiloDeWeerdt@users.noreply.github.com>
2024-05-18 22:57:49 +0200

    Enemies can walk

--- Assets/Scripts/Actors/Enemy.cs
+++ Assets/Scripts/Actors/Enemy.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(Actor))]
+[RequireComponent(typeof(Actor), typeof(AStar))]
 public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+    public Actor Target;  
+    public bool IsFighting = false;  
+    private AStar algorithm;
 
-    
-        private void Start()
+
+    private void Start()
         {
-            GameManager.Get.AddEnemy(GetComponent<Actor>());
-
+        algorithm = GetComponent<AStar>();
+        GameManager.Get.AddEnemy(GetComponent<Actor>());
         }
 
     
@@ -19,6 +21,51 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RunAI();
+    }
+
+    public void MoveAlongPath(Vector3Int targetPosition)
+
+    {
+
+        Vector3Int gridPosition = MapManager.Get.FloorMap.WorldToCell(transform.position);
+
+        Vector2 direction = algorithm.Compute((Vector2Int)gridPosition, (Vector2Int)targetPosition);
+
+        Action.Move(GetComponent<Actor>(), direction);
+
+    }
+    public void RunAI()
+
+    {
+
+        // TODO: If target is null, set target to player (from gameManager) 
+
+        if (Target == null)
+        {
+            Target = GameManager.Get.Player;
+        }
+
+        // TODO: convert the position of the target to a gridPosition 
+
+        Vector3Int gridPosition = MapManager.Get.FloorMap.WorldToCell(Target.transform.position);
+
+        // First check if already fighting, because the FieldOfView check costs more cpu 
+
+        if (IsFighting || GetComponent<Actor>().FieldOfView.Contains(gridPosition))
+
+        {
+
+            // TODO: If the enemy was not fighting, is should be fighting now 
+            if (!IsFighting)
+            {
+                IsFighting = true;
+            }
+
+
+            // TODO: call MoveAlongPath with the gridPosition 
+            MoveAlongPath(gridPosition);
+        }
+
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
     public List<Actor> Enemies = new List<Actor>();
-
+    public Actor Player { get; set; }
     
     private void Awake()
     {
@@ -27,12 +27,45 @@ public class GameManager : MonoBehaviour
 
     public Actor GetActorAtLocation(Vector3 location)
     {
-        return null;
+        if (location == Player.transform.position)
+        {
+            return Player;
+        }
+        else
+        {
+            foreach (Actor enemy in Enemies)
+            {
+                if (location == enemy.transform.position)
+                {
+                    return enemy;
+                }
+            }
+            return null;
+        }
     }
     public GameObject CreateActor(string name, Vector2 position)
     {
         GameObject actor = Instantiate(Resources.Load<GameObject>($"Prefabs/{name}"), new Vector3(position.x + 0.5f, position.y + 0.5f, 0), Quaternion.identity);
+        if (name == "Player")
+        {
+            Player = actor.GetComponent<Actor>();
+        }
+        else
+        {
+            AddEnemy(actor.GetComponent<Actor>());
+        }
         actor.name = name;
         return actor;
+    }
+    public void StartEnemyTurn()
+    {
+        foreach (Actor enemy in Enemies)
+        {
+            Enemy enemyComponent = enemy.GetComponent<Enemy>();
+            if (enemyComponent != null)
+            {
+                enemyComponent.RunAI();
+            }
+        }
     }
 }
